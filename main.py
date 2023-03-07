886619b8e003526553f43743d5328c8b050cc3eb
MaskeredeZorro <Alexanderstrarup@gmail.com>
2023-03-07 21:12:28 +0100

    Initial update to data.json and main.py structure

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+import pyomo.environ as pyomo       # Used to model the IP
+import readAndWriteJson as rwJson   # Used for reading the data file in Json format
+import matplotlib.pyplot as plt     # Used for plotting the result
 
 
-# Press the green button in the gutter to run the script.
+def readData(filename: str) -> dict:
+    data = rwJson.readJsonFileToDictionary(filename)
+    return data
+
+
+def buildModel(data: dict) -> pyomo.ConcreteModel():
+    model = pyomo.ConcreteModel()
+
+
+    return model
+
+
+def solveModel(model: pyomo.ConcreteModel()):
+    solver = pyomo.SolverFactory('gurobi')
+    solver.solve(model, tee=True)
+
+
+
+
+def displaySolution(model: pyomo.ConcreteModel(), data: dict):
+    print('Solution value is:', pyomo.value(model.obj))
+
+
+
+def main(filename: str):
+    data = readData(filename)
+    model = buildModel(data)
+    solveModel(model)
+    displaySolution(model, data)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main("data")
