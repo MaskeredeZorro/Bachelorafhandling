a30fd7bd47a6699d629959555c4ceec605001b09
MaskeredeZorro <Alexanderstrarup@gmail.com>
2023-03-09 17:40:10 +0100

    Testet forskellige efterspørgsler og tilføjet mulighed for at teste ved tilfældige efterspørgsler af produkterne. Fikset så modellen kun allokerer ventilhuse til de produkter med efterspørgsel

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pyomo.environ as pyomo       # Used to model the IP
 import readAndWriteJson as rwJson   # Used for reading the data file in Json format
 import matplotlib.pyplot as plt     # Used for plotting the result
-import numpy as np
+import random
 
 def readData(filename: str) -> dict:
     data = rwJson.readJsonFileToDictionary(filename)
@@ -18,9 +18,11 @@ def buildModel(data: dict) -> pyomo.ConcreteModel():
     model.ventilhus = data["ventilhus"]
     model.produktionstid = data["produktionstid"]
     model.produkter = data["produkter"]
-    model.efterspørgsel=data["demand"]
+    model.efterspørgsel=data["demandsommer"]
+    #model.efterspørgsel = [data["demand"][v] * random.randint(1, 1000000) for v in range(0, len(data["demand"]))]
     model.r = data["r"]
     model.a=data["a"]
+    model.w=data["w"]
     model.K = data["var_cost"]
     model.kfm = data["var_cost"]
     model.kfs = data["var_cost"]
@@ -28,6 +30,8 @@ def buildModel(data: dict) -> pyomo.ConcreteModel():
     model.produkter_længde = range(0, len(model.produkter))
     model.delta=[model.efterspørgsel[v]*model.r[v] for v in range(0, len(model.efterspørgsel))]
     #model.delta=[data["demand"][i]*data["r"] for i in range(len(data["demand"]))] #sunes løsning er blevet replicated men med vores parameternavne
+
+
 
     #Variablerne bliver defineret i modellen
     model.x = pyomo.Var(model.ventilhus_længde, model.produkter_længde, within=pyomo.Binary)
@@ -39,7 +43,9 @@ def buildModel(data: dict) -> pyomo.ConcreteModel():
             sum(model.delta[p] * model.x[v, p] for p in model.produkter_længde) == model.rho[v]
         )
 
-    #Objektfunktionen tilføjes til modellen
+    #Objektfunktionen tilføjes til modellen %%%%%%% Der er nogle produkters brug af ventilhuse som tvinger åbningen af ventilhuse, desværre. Og så kan de produkter som kan bruge det ventilhus som bliver tvunget åbent, lige så godt anvende det såfremt det er billigere at anvende.
+    #Jeg har kørt en random efterspørgsel et par gange og den finder den samme løsning lige meget hvad... Den løsning som den også finder ved 54 ventilhuse... Måske noget som er værd at undersøge hvorfor det er sådan.
+    #Glæder mig til vi får CO2e objektivet med ind over..
     model.obj=pyomo.Objective(
         expr=sum(model.opstillingspris[v]*model.y[v] for v in model.ventilhus_længde)
              +sum(model.rho[v]*model.indkøbspris[v] for v in model.ventilhus_længde)
@@ -48,17 +54,19 @@ def buildModel(data: dict) -> pyomo.ConcreteModel():
     #Anden objektfunktion tilføjes til modellen
     #model.obj=pyomo.Objective(
     #    expr=sum(model.rho[v]*model.w[v]*model.kfm for v in model.ventilhus_længde)
-    #         +sum(model.rho[v]*model.produktionstid[v]*model.K[v]*model.kfs[v] for v in model.ventilhus_længde)
+    #         +sum(model.rho[v]*model.produktionstid[v]*model.K*model.kfs for v in model.ventilhus_længde)
     #)
 
 
     #Begræns brugen af ventilhuse så kun ventilhuse som passer til et produkt, kan tildeles til det produkt
+    #Begrænsningen tager ikke højde for, at hvis der ikke er efterspørgsel så skal et produkt ikke dækkes.
     model.setcovering = pyomo.ConstraintList()
     for v in model.ventilhus_længde:
-        for p in model.produkter_længde:
-            model.setcovering.add(
-                expr=model.x[v, p] <= model.a[v][p]
-            )
+        if model.efterspørgsel[v]>0:
+            for p in model.produkter_længde:
+                model.setcovering.add(
+                    expr=model.x[v, p] <= model.a[v][p]
+                )
 
 
     #Mindst en tildelt ventilhus til hvert produkt
@@ -97,12 +105,13 @@ def displaySolution(model: pyomo.ConcreteModel(), data: dict):
         if pyomo.value(model.y[v])==1:
             print(f"Der skal anvendes {pyomo.value(model.rho[v])} enheder af ventilhus {model.y[v]}")
 
-
+    print('Solution value is:', pyomo.value(model.obj))
+    print(f"Der indkøbes i alt {sum(pyomo.value(model.rho[v]) for v in model.ventilhus_længde)}")
     print("")
     print('Følgende ventilhuse bliver anvendt i løsningen:')
     for v in model.ventilhus_længde:
         if pyomo.value(model.y[v]) == 1:
-            print(model.ventilhus[v], end=',')
+            print(model.ventilhus[v], end=', ')
     print("")
     print('\nProdukterne er dækket som følgende:')
     for p in model.produkter_længde:
