b235a41bd297a966c2f7c6e4e482b04a2655d3f0
KaptajnKlo <elisa.leineweber@gmail.com>
2023-04-05 17:46:08 +0200

    Rettelser

--- main.py
+++ main.py
@@ -31,15 +31,6 @@ def buildModel(data: dict) -> pyomo.ConcreteModel():
     model.delta=[model.efterspørgsel[v]*model.r[v] for v in range(0, len(model.efterspørgsel))]
     #model.delta=[data["demand"][i]*data["r"] for i in range(len(data["demand"]))] #sunes løsning er blevet replicated men med vores parameternavne
 
-
-    ##Test print
-    #print("Printing weights in kg")
-    #for v in model.ventilhus_længde:
-    #    print(model.w[v])
-    #print("Printing production time in hours")
-    #for v in model.ventilhus_længde:
-    #    print(model.produktionstid[v])
-
     #Variablerne bliver defineret i modellen
     model.x = pyomo.Var(model.ventilhus_længde, model.produkter_længde, within=pyomo.Binary)
     model.y = pyomo.Var(model.ventilhus_længde, within=pyomo.Binary)
@@ -54,16 +45,16 @@ def buildModel(data: dict) -> pyomo.ConcreteModel():
     #Objektfunktionen tilføjes til modellen %%%%%%% Der er nogle produkters brug af ventilhuse som tvinger åbningen af ventilhuse, desværre. Og så kan de produkter som kan bruge det ventilhus som bliver tvunget åbent, lige så godt anvende det såfremt det er billigere at anvende.
     #Jeg har kørt en random efterspørgsel et par gange og den finder den samme løsning lige meget hvad... Den løsning som den også finder ved 54 ventilhuse... Måske noget som er værd at undersøge hvorfor det er sådan.
     #Glæder mig til vi får CO2e objektivet med ind over..
-    model.obj=pyomo.Objective(
-        expr=sum(model.opstillingspris[v]*model.y[v] for v in model.ventilhus_længde)
-             +sum(model.rho[v]*model.indkøbspris[v] for v in model.ventilhus_længde)
-    )
+    #model.obj=pyomo.Objective(
+        #expr=sum(model.opstillingspris[v]*model.y[v] for v in model.ventilhus_længde)
+             #+sum(model.rho[v]*model.indkøbspris[v] for v in model.ventilhus_længde)
+    #)
 
     #Anden objektfunktion tilføjes til modellen
-    #model.obj=pyomo.Objective(
-    #    expr=sum(model.rho[v]*model.w[v]*model.kfm for v in model.ventilhus_længde)
-    #         +sum(model.rho[v]*model.produktionstid[v]*model.K*model.kfs for v in model.ventilhus_længde)
-    #)
+    model.obj=pyomo.Objective(
+        expr=sum(model.rho[v]*model.w[v]*model.kfm for v in model.ventilhus_længde)
+             +sum(model.rho[v]*model.produktionstid[v]*model.K*model.kfs for v in model.ventilhus_længde)
+    )
 
 
     #Begræns brugen af ventilhuse så kun ventilhuse som passer til et produkt, kan tildeles til det produkt
@@ -91,21 +82,19 @@ def buildModel(data: dict) -> pyomo.ConcreteModel():
         for p in model.produkter_længde:
             model.ventilhusanvendelse.add(expr=model.x[v, p] <= model.y[v])
 
+    #model.epsilonconstraintC=pyomo.ConstraintList()
+    #model.epsilonconstraintC.add(expr=(sum(model.rho[v]*model.w[v]*model.kfm for v in model.ventilhus_længde)
+                                     #+sum(model.rho[v]*model.produktionstid[v]*model.K*model.kfs for v in model.ventilhus_længde))>=617.7)
 
-    #model.epsilonconstraint=pyomo.ConstraintList()
-    #model.epsilonconstraint.add(expr=(sum(model.rho[v]*model.w[v]*model.kfm for v in model.ventilhus_længde)
-    #                                 +sum(model.rho[v]*model.produktionstid[v]*model.K*model.kfs for v in model.ventilhus_længde))<=680)
+    model.epsilonconstraintO=pyomo.ConstraintList()
+    model.epsilonconstraintO.add(expr=(sum(model.opstillingspris[v]*model.y[v] for v in model.ventilhus_længde)
+             +sum(model.rho[v]*model.indkøbspris[v] for v in model.ventilhus_længde))>=69848)
 
     return model
-
-
 
 def solveModel(model: pyomo.ConcreteModel()):
     solver = pyomo.SolverFactory('gurobi')
     solver.solve(model, tee=True)
-
-
-
 
 def displaySolution(model: pyomo.ConcreteModel(), data: dict):
     print('Solution value is:', pyomo.value(model.obj))
@@ -113,6 +102,10 @@ def displaySolution(model: pyomo.ConcreteModel(), data: dict):
     print("CO2e Udslip i kg")
     print(sum(pyomo.value(model.rho[v])*model.w[v]*model.kfm for v in model.ventilhus_længde)
              +sum(pyomo.value(model.rho[v])*model.produktionstid[v]*model.K*model.kfs for v in model.ventilhus_længde))
+
+    print("Omkostninger i Kr")
+    print(sum(pyomo.value(model.y[v])*model.opstillingspris[v] for v in model.ventilhus_længde)
+              +sum(pyomo.value(model.rho[v])*model.indkøbspris[v] for v in model.ventilhus_længde))
 
     for v in model.y:
         if pyomo.value(model.y[v])==1:
